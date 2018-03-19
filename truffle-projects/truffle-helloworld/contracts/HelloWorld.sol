36bc0ea54dd488cad0142b757f048abdda6a2e0d
cryptoseabook <cryptoseabook@gmail.com>
2018-03-19 21:51:39 +1100

    add smart contract code

---
+++ truffle-projects/truffle-helloworld/contracts/HelloWorld.sol
@@ -0,0 +1,19 @@
+pragma solidity  ^0.4.18;
+
+contract HelloWorld {
+
+    string message;
+
+    function HelloWorld() public  {
+        message = "Hello World";
+    }
+
+    function displayMessage() view public returns (string) {
+      return  message;
+    }
+
+    function setMessage(string aMessage) public {
+        message = aMessage;
+    }
+
+  }
